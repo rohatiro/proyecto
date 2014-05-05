76d8fb1515e4c2757bc7e0c67532b76f66ad60e1
Roberto Haziel Tienda Rodríguez <rohatiro@gmail.com>
2014-05-04 19:53:43 -0500

    Recivir datos de formularios, Servir archivos estaticos y primer stylus

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var express, server, swig, path, port;
+var express, server, swig, path, port, bodyParser;
 
-var rootdir, viewsdir;
+var rootdir, viewsdir, staticdir;
 
 path = require('path');
 express = require('express');
+bodyParser = require('body-parser');
 swig = require('swig');
 server = express();
 
 rootdir = path.resolve(__dirname, '.');
 viewsdir = path.resolve(rootdir, './views');
+staticdir = path.resolve(rootdir, './static');
 
 port = 3000;
 
@@ -16,8 +18,15 @@ server.engine('html', swig.renderFile);
 server.set('view engine', 'html');
 server.set('views', viewsdir);
 
+server.use(express.static(staticdir));
+server.use(bodyParser());
+
 server.get('/', function(req, res) {
 	res.render('index');
+});
+
+server.post('/parse', function(req, res) {
+	res.send('listo');
 });
 
 server.listen(port);
